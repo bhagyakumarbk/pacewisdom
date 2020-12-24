61cb11036a1dffc130fe5279b44abf76b7e953c0
bhagyakumnar <bhagyakumar.kolluru@gmail.com>
2020-12-24 12:59:57 +0530

    Pace Wise

--- src/test/java/com/pace/wisdom/PaceWisdomApplicationTests.java
+++ src/test/java/com/pace/wisdom/PaceWisdomApplicationTests.java
@@ -2,6 +2,8 @@ package com.pace.wisdom;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,18 +20,31 @@ class PaceWisdomApplicationTests {
 	public void testAddEmployeeData(){
 		EmployeeData eData=insertData();
 		String response = eService.createEmployee(eData);
+		System.out.println(response);
 		assertTrue(response!=null);
 
 		
 	}
 	@Test
 	public void testGetdEmployeeData(){
+		EmployeeData eData=insertData();
+		EmployeeData response = eService.findEmployeeById(eData.geteId());
+		assertTrue(response!=null);
 		
 	}@Test
 	public void testUpdateEmployeeData(){
 		
+		EmployeeData eData=insertData();
+		eData.setFirstName("Abraham");
+		String response = eService.updateEmployee(eData.geteId(), eData);
+		assertTrue(response!=null);
+
+		
 	}@Test
 	public void testDeleteEmployeeData(){
+		EmployeeData eData=insertData();
+		String response = eService.deleteEmployee(eData.geteId());
+		assertTrue(response!=null);
 		
 	}
 	
