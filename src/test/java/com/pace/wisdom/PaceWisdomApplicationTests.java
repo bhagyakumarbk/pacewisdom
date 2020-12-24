c93ed2ae0893d34173ae524ad9a908fce60fed0b
bhagyakumnar <bhagyakumar.kolluru@gmail.com>
2020-12-24 10:22:02 +0530

    pacewisdom

--- src/test/java/com/pace/wisdom/PaceWisdomApplicationTests.java
+++ src/test/java/com/pace/wisdom/PaceWisdomApplicationTests.java
@@ -1,13 +1,48 @@
 package com.pace.wisdom;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.pace.wisdom.api.EmployeeData;
+import com.pace.wisdom.service.EmployeeService;
 
 @SpringBootTest
 class PaceWisdomApplicationTests {
-
+	
+	@Autowired
+	EmployeeService eService;
 	@Test
-	void contextLoads() {
-	}
+	public void testAddEmployeeData(){
+		EmployeeData eData=insertData();
+		String response = eService.createEmployee(eData);
+		assertTrue(response!=null);
 
+		
+	}
+	@Test
+	public void testGetdEmployeeData(){
+		
+	}@Test
+	public void testUpdateEmployeeData(){
+		
+	}@Test
+	public void testDeleteEmployeeData(){
+		
+	}
+	
+	private EmployeeData insertData() {
+		EmployeeData eData=new EmployeeData();
+		eData.seteId(2255);
+		eData.setFirstName("Jonus");
+		eData.setLastName("Khanwald");
+		eData.setAddress("Winden");
+		eData.setAge(24);
+		eData.setEmailId("jhonkhanwald@gmail.com");
+		eData.setGender("Male");
+		
+		return eData;
+	}
 }
